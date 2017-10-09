51cb54838ddd49f4f5ec866146e262dbefa0fcc2
Keith Salisbury <keithsalisbury@gmail.com>
2017-10-09 01:54:20 +0100

    Update for deployment

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Draw
 
-**TODO: Add description**
+This is a simple web toy created using:
 
+- Elixir https://elixir-lang.org/
+- Phoenix Framework http://phoenixframework.org/
+- D3.js https://d3js.org/
+
+The demo takes advantage of Phoenix Presence.
+
+An example of this is installed https://draw.globalkeith.com
--- apps/draw_web/config/prod.exs
+++ apps/draw_web/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :draw_web, DrawWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOSTNAME"), port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
